befd25f19716c78bd70ba30bb678c754b71d630f
LubovKannika <82404393+LubovKannika@users.noreply.github.com>
2021-07-07 23:57:31 +0300

    Initial Commit

---
+++ Homework-1-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-1-2
+//
+//  Created by Lubov Kannika on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
